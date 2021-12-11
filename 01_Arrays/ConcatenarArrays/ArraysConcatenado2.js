64914e11d6b99dbd3d370e1d0670e260e25b18cb
Claudeilton Dantas <fcd007@hotmail.com>
2021-12-10 21:46:37 -0300

    add files testes

---
+++ 01_Arrays/ConcatenarArrays/ArraysConcatenado2.js
@@ -0,0 +1,16 @@
+let pessoa = {
+  nome: undefined,
+  idade: undefined,
+  altura: undefined,
+  cpf: undefined,
+  endereco: undefined
+}
+
+let endereco = {
+  logradouro: '',
+  numero: undefined,
+  CEP: '',
+  cidade: undefined,
+  estado: undefined,
+  pais: undefined
+}
